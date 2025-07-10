26c40248b908c0d5ca50bc11b579476d710ba53b
Zen T <ztang230@gmail.com>
2025-07-10 09:52:11 -0500

    quickstart for python package!

---
+++ test-assembler/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from test-assembler!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ test-assembler/packages/ara/ara/memory.py
@@ -0,0 +1,6 @@
+class Memory:
+    def __init__(self):
+        print("Memory initialized!")
+        
+    def testitout(self):
+        print("Testing it out! Hot reload is working!!! we have a new change!")
